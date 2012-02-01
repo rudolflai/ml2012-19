2f77f821ffc25d633852f070ddb7333102c5e6fb
Shawn Kwek <angstikira@gmail.com>
2012-02-01 03:44:07 +0000

    Initial addition of makeTree function and helper

---
+++ makeTree.m
@@ -0,0 +1,11 @@
+function [ tree ] = makeTree( samples, attributes, targets )
+%MAKETREE Creates the decision tree
+%   samples is the matrix of AUs activation
+%   attributes is the list of AUs yet to be used for learning
+%   targets is the list of remapped labels (1s/0s)
+
+if targets
+
+
+end
+
