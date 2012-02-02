fcdfdd438803de89de9e8b31189af15c7dfb47b4
Shawn Kwek <angstikira@gmail.com>
2012-02-02 00:54:07 +0000

    Proper working code now. Not fully tested for corner cases Is able to produce a decision tree No code to link up the functions. Need to be called from command window Dummy code in best_attribute file. Note the order of parameters in the signature is slightly changed Removed unused code

--- makeTreeForLabel.m
+++ makeTreeForLabel.m
@@ -2,7 +2,7 @@ function [ tree ] = makeTreeForLabel( samples, targets, target_emotion )
 %MAKETREEFORLABEL Builds a tree depending on the target emotion
 %   Detailed explanation goes here
 
-tree = makeTree(samples,1:45,targets == target_emotion);
+tree = makeTree(samples,1:size(samples,2),targets == target_emotion);
 
 
 end
--- sieve.m
+++
@@ -1,7 +0,0 @@
-function [ outrow ] = sieve( attribute, value, inrow)
-    if inrow(attribute) == value
-        outrow = inrow;
-    else
-        outrow = [];
-    end
-end
