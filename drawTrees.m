217a90fbd1d3776723d418174d3db2b53d26fd41
Shawn Kwek <angstikira@gmail.com>
2012-02-19 22:42:22 +0000

    Removed unnecessary files

--- drawTrees.m
+++
@@ -1,9 +0,0 @@
-function [ output_args ] = drawTrees( trees )
-%DRAWTREES Draws the input list of trees
-%   trees: List of trees which adheres to the standard given in the CBC
-%       manual
-
-arrayfun(@(x) DrawDecisionTree(trees(x), strcat('Decision tree for: ',emolab2str(x))),1:6)
-
-end
-
--- majorityValue.m
+++
@@ -1,8 +0,0 @@
-function [ majority ] = majorityValue( targets )
-% MAJORITYVALUE - Summary of this function goes here
-% Detailed explanation goes here
-
-majority = round(mean(targets));
-
-end
-
