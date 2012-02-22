34c12fca0ba3d8d960a0721052d537c488624844
Charles Koh <czwkoh@gmail.com>
2012-02-22 22:35:22 +0000

    made testsingaleANN get raw values

--- testSingleANN.m
+++ testSingleANN.m
@@ -1,8 +1,14 @@
 function [ pred ] = testSingleANN( net, au )
 %TESTSINGLEANN same as test ANN but uses results from a single output ANN
-    [pred, tr] = sim(net,au');
-    [v I] = max(pred);
-    pred = I==1;
+    [pred1, tr] = sim(net,au');
+    [v I] = max(pred1);
+    entry = size(I,2);
+    pred = zeros(1,entry);
+    for col=1:entry
+        if(I(col)==1)
+            pred(col) = pred1(I(col),col);
+        end
+    end
 
 end
 
