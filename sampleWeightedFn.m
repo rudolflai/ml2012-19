0fc98968a393375fe7496b3c7de2bc6ea008af91
Darcy Qiu <darcy.qiu@gmail.com>
2012-03-04 17:44:44 +0000

    Minor changes

--- sampleWeightedFn.m
+++ sampleWeightedFn.m
@@ -1,9 +1,9 @@
 % xAUs: [1 x 45]
 % yAUs: [1 x 45]
 % distFn: distance function handle
-function [ w ] = sampleWeightedFn( xAUs, yAUs, distFn )
+function [ w ] = sampleWeightedFn( xAUs, yAUs )
 
-w = 1 / distFn(xAUs, yAUs)^2;
+w = 1 / getEuclideanDistance(xAUs, yAUs)^2;
 
 end
 
