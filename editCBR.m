98330a0930c9cd06672be0568f65a40d4f6b977e
Shawn Kwek <angstikira@gmail.com>
2012-03-04 20:40:59 +0000

    Cleaned up folder directory and added skeleton file

---
+++ editCBR.m
@@ -0,0 +1,8 @@
+function [ y ] = editCBR( cbr, x )
+%EDITCBR Produces a vector of label predictions y in the format of loaddata
+%   cbr: trained CBR
+%   x: features x as returned by loaddata
+
+
+end
+
