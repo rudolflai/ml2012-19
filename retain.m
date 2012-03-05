7251ff40d118305d4d91c5faed58f87cbc24435b
Rudolf Lai <hello@rudolflai.com>
2012-03-05 18:00:36 +0000

    Fixed some syntax issues for RETAIN

--- retain.m
+++ retain.m
@@ -1,10 +1,10 @@
 function [ cbr ] = retain( cbr, solvedcase )
-%RETAIN Updates the CBR system
+% RETAIN Updates the CBR system
 %   cbr: current CBR system
 
-exist = 0;
+  exist = 0;
 
-for i = 1:length(cbr.emotion)
+  for i = 1:length(cbr.emotion)
     % if the problem is not a subset of the index of the emotion, it is not
     % stored in this cluster. Small heuristic to speed up the search.
     if (sum(ismember(solvedcase.problem, cbr.emotion{i}.index)) ...
@@ -15,24 +15,24 @@ for i = 1:length(cbr.emotion)
         break;
     end
     for j = 1:length(cbr.emotion{i}.cases)
-        current_case = cbr.emotion{i}.cases(j);
-        if (isequal(current_case.problem, solvedcase.problem))
-            current_case.typicality = current_case.typicality + 1;
-            if (solvedcase.solution > 0)
-                current_case.solution = solvedcase.solution;
-            end
-            exist = 1;
-            break;
+      current_case = cbr.emotion{i}.cases(j);
+      if (isequal(current_case.problem, solvedcase.problem))
+        current_case.typicality = current_case.typicality + 1;
+        if (solvedcase.solution > 0)
+          current_case.solution = solvedcase.solution;
         end
+        exist = 1;
+        break;
+      end
     end
-end
+  end
 
-if (exist == 0)
+  if (exist == 0)
     cbr.emotion{solvedcase.solution}.cases = ...
         [cbr.emotion{solvedcase.solution}.cases; solvedcase];
     cbr.emotion{solvedcase.solution}.index = ...
         union(cbr.emotion{solvedcase.solution}.index, solvedcase.problem);
-end
+  end
 
 end
 
