a3b808827ff474b8cd19d487c1ea660e0d04231c
Shawn Kwek <angstikira@gmail.com>
2012-03-05 16:05:35 +0000

    Extracted the secondary functon cbr_add to retain.m

--- CBRinit.m
+++ CBRinit.m
@@ -6,7 +6,7 @@ function [cbr] = CBRinit (examples, targets)
   cbr = cbr_create(length(unique(targets)));
   for i = 1:size(examples,1)
     new_case = cbr_new_case(examples(i, :), targets(i, :));
-    cbr      = cbr_add(cbr, new_case);
+    cbr      = retain(cbr, new_case);
   end
 end
 
@@ -17,36 +17,4 @@ function [cbr] = cbr_create (emotion_count)
   end
 end
 
-function [cbr] = cbr_add (cbr, new_case)
-  exist = 0;
 
-  for i = 1:length(cbr.emotion)
-    % if the problem is not a subset of the index of the emotion, it is not
-    % stored in this cluster. Small heuristic to speed up the search.
-    if (sum(ismember(new_case.problem, cbr.emotion{i}.index)) ... 
-        ~= length(new_case.problem))
-      continue;
-    end
-    if (exist == 1) 
-      break;
-    end
-    for j = 1:length(cbr.emotion{i}.cases)
-      current_case = cbr.emotion{i}.cases(j);
-      if (isequal(current_case.problem, new_case.problem))
-        current_case.typicality = current_case.typicality + 1;
-        if (new_case.solution > 0)
-          current_case.solution = new_case.solution;
-        end
-        exist = 1;
-        break;
-      end
-    end
-  end
-  
-  if (exist == 0)
-    cbr.emotion{new_case.solution}.cases = ... 
-      [cbr.emotion{new_case.solution}.cases; new_case];
-    cbr.emotion{new_case.solution}.index = ... 
-      union(cbr.emotion{new_case.solution}.index, new_case.problem);
-  end
-end
--- retain.m
+++ retain.m
@@ -2,7 +2,37 @@ function [ cbr ] = retain( cbr, solvedcase )
 %RETAIN Updates the CBR system
 %   cbr: current CBR system
 
+exist = 0;
 
+for i = 1:length(cbr.emotion)
+    % if the problem is not a subset of the index of the emotion, it is not
+    % stored in this cluster. Small heuristic to speed up the search.
+    if (sum(ismember(solvedcase.problem, cbr.emotion{i}.index)) ...
+            ~= length(solvedcase.problem))
+        continue;
+    end
+    if (exist == 1)
+        break;
+    end
+    for j = 1:length(cbr.emotion{i}.cases)
+        current_case = cbr.emotion{i}.cases(j);
+        if (isequal(current_case.problem, solvedcase.problem))
+            current_case.typicality = current_case.typicality + 1;
+            if (solvedcase.solution > 0)
+                current_case.solution = solvedcase.solution;
+            end
+            exist = 1;
+            break;
+        end
+    end
+end
+
+if (exist == 0)
+    cbr.emotion{solvedcase.solution}.cases = ...
+        [cbr.emotion{solvedcase.solution}.cases; solvedcase];
+    cbr.emotion{solvedcase.solution}.index = ...
+        union(cbr.emotion{solvedcase.solution}.index, solvedcase.problem);
+end
 
 end
 
