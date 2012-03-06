d2cdb46223c9dcdc5d53f288e805a4230f54c69f
Darcy Qiu <darcy.qiu@gmail.com>
2012-03-06 12:46:18 +0000

    Added impl for retrieve()

--- retrieve.m
+++ retrieve.m
@@ -1,8 +1,16 @@
-function [ closestcase ] = retrieve( cbr, newcase )
-%RETRIEVE Returns the case which is closest to new case
-%   cbr: non-empty CBR system
-%   newcase: case w/o a solution
+% Returns the case which is closest to new case
+% cbr_sys: non-empty cbr_sys system
+% newcase: case without a solution
 
+function [ closestcase ] = retrieve( cbr_sys, newcase )
+
+k = 5;
+distFn = @getEuclideanDistance;
+weightedFn = @euclideanWeightedFn;
+
+matrixAUs = reshape([cbr_sys.problem], length(cbr_sys), 45);
+kNN(k, [newcase.problem], matrixAUs, [cbr_sys.solution], distFn, ...
+    weightedFn, [cbr_sys.typicality]);
 
 end
 
