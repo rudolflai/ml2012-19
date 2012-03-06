526d64777ea8fad834ac675ea27af49957aafec0
Charles Koh <czwkoh@gmail.com>
2012-03-06 20:29:32 +0000

    the testweightdistfn now works but is random results hard to say what are the best functions

--- TestWeightDistFn.m
+++ TestWeightDistFn.m
@@ -1,4 +1,4 @@
-function [f1Measures] = TestWeightDistFn(examples, targets)
+function [f1Measures,cms] = TestWeightDistFn(examples, targets)
 %TestWeightDistFn : Does the ten fold validation.
 %Returns the error estimate.
 %Inputs are the examples and the EXPECTED targets of the emotions that
@@ -12,18 +12,40 @@ function [f1Measures] = TestWeightDistFn(examples, targets)
 kLimit = 10;
 
 f1Measures = zeros(kLimit,3,3);
+% cells to hold matrixes
+cms = cell(kLimit,3,3);
+for k=1:kLimit
+    for ret=1:3
+        for get=1:3
+            cms{k,ret,get} = zeros(6,6);
+        end
+    end
+end
 
 %First find size of training data
 cases = size(examples,1);
 
 %Checks if valid for 10 fold validation
-if(cases >= 10)
+if(cases < 10)
+    error('Too few cases to perform ten fold validation with');
+end
+
+
+%First find size of training data
+cases = size(examples,1);
+
+%Checks if valid for 10 fold validation
+if(cases < 10)
     error('Too few cases to perform ten fold validation with');
 end
 
 %Find the (maximum) no. of entries per test data set
 %Test set takes 10% of full dataset
 testsize = ceil(cases/10);
+
+%default value
+total.cm = zeros(6,6);
+perfold = zeros(10,2);
 
 %Randomize the order of targets and examples once
 order = randperm(cases);
@@ -34,51 +56,30 @@ randtargets = targets(order);
 for foldcount=1:10,
     
     %Subdivide the dataset into test and training sets
-    testlogic = zeros(cases,1);
-    testlogic(1 + (foldcount - 1) * testsize : foldcount * testsize) = 1;
+    testlogic = false(cases,1);
+    testlogic(1 + (foldcount - 1) * testsize : foldcount * testsize) =...
+        true;
     
     testset = randexamples(testlogic,:);
     testansset = randtargets(testlogic);
     
     trainexamples = randexamples(~testlogic,:);
     traintargets = randtargets(~testlogic);
-    cms = cell(kLimit,3,3);
-    count = 0;
-    %Train a CBR system
     for k=1:kLimit
         for ret=1:3
             for get=1:3
-                if(count<(kLimit*3*3))
-                    count = count+1;
-                    cms{k,ret,get}=zeroes(6,6);
-                end
+                %Train a CBR system
                 cbr = CBRinit(trainexamples, traintargets);
-                cases = size(testset,1);
-                randcases = randperm(cases);
-                labels = zeros(cases,1);
-                for i=randcases,
-                    newcase = cbr_new_case (testset(), 0);
-                    oldcase;
-                    if(ret==1)
-                        oldcase = retrieveCityBlock(cbr,newcase,k,get);
-                    else
-                        if(ret==2)
-                            oldcase = retrieveChebyshev(cbr,newcase,k,get);
-                        else
-                            oldcase = retrieveEuclidean(cbr,newcase,k,get);
-                        end
-                    end
-                    solvedcase = reuse(oldcase,newcase);
-                    cbr = retain(cbr, solvedcase);
-                    labels(i) = solvedcase.solution;
-                    cms{k,ret,get} = cms{k,ret,get}+ ...
-                        ConfusionMatrix(testansset, labels);
-                end
+                
+                %Generate CM for this fold                
+                cms{k,get,ret} = cms{k,get,ret} + ...
+                    sndExamples2CM(cbr, testset, testansset,k,ret,get);
+                
             end
         end
-        
     end
 end
+
 highestf1=0;
 highestK=0;
 highestRet=0;
@@ -86,11 +87,10 @@ highestGet=0;
 for k=1:kLimit
     for ret=1:3
         for get=1:3
-            averagerp = mean(CM2RP(cms{k,ret,get}),1);
-            rc = averagerp(1);
-            prc = averagerp(2);
-            f1Measures(k,ret,get) = 2 * rc * prc / ( rc + prc);
-            if(f1>highestf1)
+            averagerp = CM2RP(cms{k,ret,get});
+            f1Measures(k,ret,get) = nanmean(averagerp(:));
+            if(f1Measures(k,ret,get)>highestf1)
+                highestf1 = f1Measures(k,ret,get);
                 highestK=k;
                 highestRet=ret;
                 highestGet=get;
@@ -102,46 +102,60 @@ Rtype = {'City Block','Chebyshev','Eulidean'};
 
 
 display(f1Measures);
-sprintf('best f1 performance by:\n');
-sprintf('k = %i\n',highestK);
-sprintf('dist fn = %s\n',Rtype{highestRet});
-sprintf('weighing fn = %s\n',Rtype{highestGet});
+fprintf('best f1 performance by:\n');
+fprintf('k = %i\n',highestK);
+fprintf('dist fn = %s\n',Rtype{highestRet});
+fprintf('weighing fn = %s\n',Rtype{highestGet});
+fprintf('f1 value = %d\n',highestf1);
 
 end
 
-function[cases] = retrieveCityBlock(cbr,newcase,k,weigh)
-weight;
+function[cm] =sndExamples2CM(cbr, x, y,k,ret,get)
+    predicted = sndtestCBR(cbr, x,k,ret,get);
+    cm = ConfusionMatrix(y, predicted);
+end
+
+function [ labels ] = sndtestCBR( cbr, x , k ,ret, get)
+  
+cases = size(x,1);
+randcaseorder = randperm(cases);
+labels = zeros(cases,1);
+
+for i=randcaseorder,
+    [labels(i),cbr] = sndtestExample(cbr, x(i,:),k,ret,get);
+end
+end
+
+function [ y, updatedcbr ] = sndtestExample( cbr, x ,k,ret,get)
+newcase = cbr_new_case (x, 0);
+solvedcase = reuse(sndretrieve(cbr, newcase,k,ret,get), newcase);
+updatedcbr = retain(cbr, solvedcase);
+y = solvedcase.solution;
+end
+
+function [ closestcase ] = sndretrieve( cbr_sys, newcase,k,ret,get)
+
 if(ret==1)
-    weight = cityBlockWeightFn(somecase.au,newcase.au);
-else
-    if(ret==2)
-        weight = chebWeightFn(somecase.au,newcase.au);
-    else
-        weight = euclideanWeightFn(somecase.au,newcase.au);
-    end
+    distFn = @getCityBlockDistance;
 end
+if(ret==2)
+    distFn = @getChebyshevDistance;
 end
-function[cases] = retrieveChebyshev(cbr,newcase,k,weigh)
-weight;
-if(ret==1)
-    weight = cityBlockWeightFn(somecase.au,newcase.au);
-else
-    if(ret==2)
-        weight = chebWeightFn(somecase.au,newcase.au);
-    else
-        weight = euclideanWeightFn(somecase.au,newcase.au);
-    end
+if(ret==3)
+    distFn = @getEuclideanDistance;
 end
+
+if(get==1)
+    weightedFn = @cityBlockWeightedFn;
 end
-function[cases] = retrieveEuclidean(cbr,newcase,k,weigh)
-weight;
-if(ret==1)
-    weight = cityBlockWeightFn(somecase.au,newcase.au);
-else
-    if(ret==2)
-        weight = chebWeightFn(somecase.au,newcase.au);
-    else
-        weight = euclideanWeightFn(somecase.au,newcase.au);
-    end
+if(get==2)
+    weightedFn = @chebWeightedFn;
 end
+if(get==3)
+    weightedFn = @euclideanWeightedFn;
+end
+matrixAUs = reshape([cbr_sys.problem], 45, length(cbr_sys))';
+closestcase = kNN(k, [newcase.problem], matrixAUs, [cbr_sys.solution], ...
+    distFn, weightedFn, [cbr_sys.typicality]);
+
 end
