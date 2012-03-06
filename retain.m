cc66fcb40dd90dce92ca59df1d1014bb4ec93a27
Darcy Qiu <darcy.qiu@gmail.com>
2012-03-06 01:49:18 +0000

    Changed the CBR organisation to flat and the retain() function
    retain() now takes in a solved case and check if it's already in the
    system. If the solved case has the same AUs and label as a case in the
    system, increments the typicality of it. Otherwise, add it into the
    system.

--- retain.m
+++ retain.m
@@ -1,38 +1,28 @@
-function [ cbr ] = retain( cbr, solvedcase )
-% RETAIN Updates the CBR system
-%   cbr: current CBR system
+% Updates the CBR system. If same case with the same label already exists 
+% in the system, increments its typicality. Otherwise, add it to the system
+%
+% cbr_sys: current CBR system
+% solved_case: case with solution
 
-  exist = 0;
+function [ cbr_sys ] = retain( cbr_sys, solved_case )
 
-  for i = 1:length(cbr.emotion)
-    % if the problem is not a subset of the index of the emotion, it is not
-    % stored in this cluster. Small heuristic to speed up the search.
-    if (sum(ismember(solvedcase.problem, cbr.emotion{i}.index)) ...
-            ~= length(solvedcase.problem))
-        continue;
-    end
-    if (exist == 1)
-        break;
-    end
-    for j = 1:length(cbr.emotion{i}.cases)
-      current_case = cbr.emotion{i}.cases(j);
-      if (isequal(current_case.problem, solvedcase.problem))
-        current_case.typicality = current_case.typicality + 1;
-        if (solvedcase.solution > 0)
-          current_case.solution = solvedcase.solution;
-        end
+% find indices of cases with same label as the solved case
+ind = find([cbr_sys.solution] == solved_case.solution);
+
+exist = 0;  % flag to check if the case has already existed.
+
+for i = ind
+    if isequal(cbr_sys(i).problem, solved_case.problem)
+        cbr_sys(i).typicality = cbr_sys(i).typicality + 1;
         exist = 1;
         break;
-      end
     end
-  end
-
-  if (exist == 0)
-    cbr.emotion{solvedcase.solution}.cases = ...
-        [cbr.emotion{solvedcase.solution}.cases; solvedcase];
-    cbr.emotion{solvedcase.solution}.index = ...
-        union(cbr.emotion{solvedcase.solution}.index, solvedcase.problem);
-  end
+end 
+    
+% if doesn't exist, simple add it to the bottom of the CBR system
+if ~exist
+    cbr_sys = [cbr_sys ; solved_case];
+end
 
 end
 
