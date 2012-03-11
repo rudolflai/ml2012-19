b29ce2ad6fb35c49229975d557cd148f177d9ed2
Charles Koh <czwkoh@gmail.com>
2012-03-11 14:57:35 +0000

    added needed stuff

--- createAllTrees.m
+++ createAllTrees.m
@@ -17,20 +17,5 @@ function [ trees ] = createAllTrees(examples, targets, tenfold)
 	  	
 trees = arrayfun(@(x) makeTreeForLabel(examples, targets, x), 1:6);
 	
-if (tenfold)
-	  	
-% Generate precision to measure the reliability of each tree
-	  	
-    [~,cm] = TenFoldValidation(examples,targets,0);
-	  	
-    rplist = CM2RP(cm);
-	  	
-    for i=1:6,
-	  	
-        trees(i).precision = rplist(i,2);
-	  	
-    end
-	  	
-end
-	  	
+
 end
