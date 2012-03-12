321bf4653dfe94e5f701d64277da8a2cfc84a4cb
Shawn Kwek <angstikira@gmail.com>
2012-03-12 13:33:58 +0000

    Changed TenFoldAllAlgos to return a 3d matrix instead

--- TenFoldAllAlgos.m
+++ TenFoldAllAlgos.m
@@ -1,6 +1,6 @@
 function [f1matrix] = TenFoldAllAlgos(clean)
-% Returns a 3 element cell array, containing all the f1 measures per fold
-% for each emotion, i.e 10 X 6. Cell order: trees, nn, cbr
+% Returns a 10 X 6 X 3 array, containing all the f1 measures per fold
+% for each emotion. Cell order: trees, nn, cbr
 if (clean)
     file = 'cleandata_students.txt';
 else
@@ -9,8 +9,11 @@ end
 
 [x, y] = loaddata(file);
 algos = {@CreateAndTestTrees @CreateAndTestNN @CreateAndTestCBR};
+name = {'Decision Trees' 'Neural Networks' 'Case Based Reasoning'};
+f1matrix = zeros(10,6,3);
 
-f1matrix = cellfun(@(algo) TenFoldValidation(x,y,algo), algos, ...
-    'UniformOutput', false);
-
+for i=1:3,
+    fprintf(name{i});
+    f1matrix(:,:,i) = TenFoldValidation(x,y,algos{i})
+end
 end
